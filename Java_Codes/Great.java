6989362ea4f76957ce15a7697558121c0a5c2046
Ryan <152311395+Vegito2005@users.noreply.github.com>
2025-02-23 08:14:13 +0300

    Create Great.java

---
+++ Java_Codes/Great.java
@@ -0,0 +1,18 @@
+package Javagames;
+
+class Great 
+{
+    public static void main(String args[])
+    {
+        int x=150;
+        int y=110;
+        int z=214;
+        if(x>y&&x>z){
+            System.out.println("x is greater");
+        }else if(y>x&&y>z){
+            System.out.println("y is greater");
+        }else{
+            System.out.println("z is greater");
+        }
+    }
+}
