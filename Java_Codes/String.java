0c5337e64a1ffb67874aa1d58adcfbd2e2d05626
Ryan <152311395+Vegito2005@users.noreply.github.com>
2025-02-23 08:21:57 +0300

    Create String.java

---
+++ Java_Codes/String.java
@@ -0,0 +1,13 @@
+package samples;
+
+public class Strconc {
+
+	public static void main(String[] args) {
+		String str1="Ryan";
+		String str2="Savio";
+		String str3="Sequeira";
+		System.out.println(str1+str2+str3);
+
+	}
+
+}
