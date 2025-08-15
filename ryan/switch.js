c2e6cccdd81ea4833d0e7aa422fca82f417e413d
Ryan <152311395+RyanSavio2005@users.noreply.github.com>
2025-08-15 10:02:12 +0530

    Add files via upload

---
+++ ryan/switch.js
@@ -0,0 +1,15 @@
+let grade='A';
+switch(grade){
+    case 'A':
+        console.log('Excellent');
+        break;
+    case 'B':
+        console.log('Good');
+        break;
+    case 'C':
+        console.log('Fair');
+        break;
+    default:
+        console.log('Fail');
+        break;
+}
